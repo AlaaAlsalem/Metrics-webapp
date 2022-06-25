15b8ca303a1730c8e2262fecd378d7d027c42b72
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-25 07:16:28 +0300

    Structure Files

---
+++ src/components/Details.js
@@ -0,0 +1,7 @@
+const Details = () => {
+    return ( 
+        <div> Hello from Details</div>
+     );
+}
+ 
+export default Details;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div>
+            Welcome from Home!!
+        </div>
+     );
+}
+ 
+export default Home;
